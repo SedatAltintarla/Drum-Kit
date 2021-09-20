61e404be92c2fd3133870f3879579d046bf66ef4
Sedat Altintarla <sedatatprivat@gmail.com>
2021-09-20 18:13:08 +0200

    add key press event

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ const drums = document.querySelectorAll(".drum");
 
 const sounds = ["crash", "kick-bass", "snare", "tom-1", "tom-2", "tom-3", "tom-4"];
 
+const keys = [];
+
 function playSound(sound) {
   let audio = new Audio(`sounds/${sound}.mp3`);
   audio.play();
 }
 
+
 for(let i = 0; i < drumsLength; i++) {
+  keys.push(drums[i].innerHTML);
     document.querySelectorAll("button")[i].addEventListener("click", function () {
         
       playSound(sounds[i]);
     
   });
 }
+
+document.addEventListener("keydown", event => {
+
+  keys.forEach(key => {
+    if(event.key == key) {
+        playSound()
+    }
+  });
+
+});
+
+console.log(keys);
