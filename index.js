78b00de3c8a42ea870e38cc9de9d1e55fea6e3d4
Sedat <sedatatprivat@gmail.com>
2021-09-24 19:22:26 +0200

    update key down event

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const drumsLength = document.querySelectorAll(".drum").length;
 const drums = document.querySelectorAll(".drum");
 
+const keys = [];
+
+const music = {
+  sounds: ["crash", "kick-bass", "snare", "tom-1", "tom-2", "tom-3", "tom-4"],
+  keys
+}
+
 const sounds = ["crash", "kick-bass", "snare", "tom-1", "tom-2", "tom-3", "tom-4"];
+
 
 function playSound(sound) {
   let audio = new Audio(`sounds/${sound}.mp3`);
   audio.play();
 }
 
+
 for(let i = 0; i < drumsLength; i++) {
+  keys.push(drums[i].innerHTML);
     document.querySelectorAll("button")[i].addEventListener("click", function () {
         
       playSound(sounds[i]);
     
   });
 }
+
+
+document.addEventListener("keydown", event => {
+
+  for(let i = 0; i < music.keys.length; i++){
+    if(event.key == music.keys[i]) {
+      playSound(music.sounds[i]);
+    }
+  }
+
+})
